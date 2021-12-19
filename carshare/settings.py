29894917f4f80fda1774e544f307dd4d540bd1c1
George Goldberg <george@appletreetechnology.com>
2021-12-19 16:42:52 +0000

    Get tailwind set up and make the very first template.
    The first template is the first question of the sign-up page: are you
    signing up for Personal or Business use.

--- carshare/settings.py
+++ carshare/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def sign_up(request):
+    context = {}
+    return render(request, 'users/signup.html', context)
