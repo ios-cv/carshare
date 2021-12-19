4872891af6fa80ce1516173b89b60308a469526a
George Goldberg <george@appletreetechnology.com>
2021-12-19 17:56:59 +0000

    Add a BillingAccount model.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,21 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class BillingAccount(models.Model):
+    BUSINESS = 'b'
+    PERSONAL = 'p'
+    ACCOUNT_TYPE_CHOICES = [
+        (BUSINESS, 'Business'),
+        (PERSONAL, 'personal'),
+    ]
+
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
+    type = models.CharField(
+        max_length=1,
+        choices=ACCOUNT_TYPE_CHOICES,
+    )
