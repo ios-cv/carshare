7c78ac39880a9b51ba5ff646ee976f3f7a05ab69
George Goldberg <george@appletreetechnology.com>
2022-04-07 20:27:54 +0100

    Proper validation on the "find a vehicle" form.

--- bookings/forms.py
+++ bookings/forms.py
@@ -3,9 +3,21 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout
 
 from django import forms
+from django.core.exceptions import ValidationError
 from django.forms import widgets
+from django.utils import timezone
 
 from hardware.models import VehicleType
+
+
+def gen_start_time(now):
+    minutes = now.minute - (now.minute % 5)
+    return now.replace(minute=minutes, second=0, microsecond=0) + timezone.timedelta(minutes=5)
+
+
+def gen_end_time(now):
+    start = gen_start_time(now)
+    return start + timezone.timedelta(hours=1)
 
 
 class VehicleTypeMultipleChoiceField(forms.ModelMultipleChoiceField):
@@ -14,11 +26,29 @@ class VehicleTypeMultipleChoiceField(forms.ModelMultipleChoiceField):
 
 
 class BookingSearchForm(forms.Form):
-    start = forms.DateTimeField(label="Start Time")
-    end = forms.DateTimeField(label="End Time")
+    start = forms.SplitDateTimeField(
+        label="Start time",
+        widget=forms.SplitDateTimeWidget(
+            date_attrs={'type': 'date'},
+            time_attrs={'type': 'time', 'step': '60'},
+            date_format="%Y-%m-%d",
+            time_format="%H:%M",
+        ),
+    )
+    end = forms.SplitDateTimeField(
+        label="End time",
+        widget=forms.SplitDateTimeWidget(
+            date_attrs={'type': 'date'},
+            time_attrs={'type': 'time', 'step': '60'},
+            date_format="%Y-%m-%d",
+            time_format="%H:%M",
+        ),
+    )
     vehicle_types = VehicleTypeMultipleChoiceField(
+        label="Type of vehicle",
         queryset=VehicleType.objects.all(),
         widget=forms.CheckboxSelectMultiple,
+        error_messages={'required': 'You must select at least one type of vehicle.'}
     )
 
     def __init__(self, *args, **kwargs):
@@ -27,7 +57,37 @@ class BookingSearchForm(forms.Form):
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.layout = Layout("start", "end", InlineCheckboxes("vehicle_types"))
+
         self.fields["vehicle_types"].initial = VehicleType.objects.all()
+
+        now = timezone.now()
+        self.fields["start"].initial = gen_start_time(now)
+        self.fields["end"].initial = gen_end_time(now)
+
+    def clean_start(self):
+        start = self.cleaned_data['start']
+
+        if start + timezone.timedelta(minutes=5) < timezone.now():
+            raise ValidationError(
+                "Your booking must not start in the past."
+            )
+
+        return start
+
+    def clean(self):
+        cleaned_data = super().clean()
+        start = cleaned_data.get("start")
+        end = cleaned_data.get("end")
+
+        if start and end and start >= end:
+            raise ValidationError(
+                "You must choose an end time after your start time."
+            )
+
+        if start and end and start + timezone.timedelta(hours=1) > end:
+            raise ValidationError(
+                "Your booking must be at least 1 hour long."
+            )
 
 
 class ConfirmBookingForm(forms.Form):
--- carshare/settings.py
+++ carshare/settings.py
@@ -153,9 +153,9 @@ LOGOUT_REDIRECT_URL = "/"
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "en-gb"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/London"
 
 USE_I18N = True
 
