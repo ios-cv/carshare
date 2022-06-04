2dceac40af64580e3cbcb5437c52ccbeb4f037f9
George Goldberg <george@appletreetechnology.com>
2022-06-04 12:54:56 +0100

    Get the whole of the driver profile form looking pretty.

--- drivers/forms.py
+++ drivers/forms.py
@@ -34,6 +34,12 @@ class DriverProfilePart1Form(forms.ModelForm):
             "date_of_birth": "Your date of birth must match that shown on your driving licence.",
             "address_line_1": "Your address and postcode must match that shown on your driving licence.",
         }
+        widgets = {
+            "date_of_birth": forms.widgets.DateInput(
+                attrs={"type": "date"},
+                format="%Y-%m-%d",
+            ),
+        }
 
     def save(self, commit=True):
         m = super().save(commit=False)
@@ -48,9 +54,24 @@ class DriverProfilePart2Form(forms.ModelForm):
         model = DriverProfile
         fields = ["licence_number", "licence_issue_date", "licence_expiry_date"]
         labels = {
-            "licence_number": "Driving Licence Number",
-            "licence_issue_date": "Driving Licence Issue Date",
-            "licence_expiry_date": "Driving Licence Expiry Date",
+            "licence_number": "Driving licence number",
+            "licence_issue_date": "Driving licence date of issue",
+            "licence_expiry_date": "Driving licence date of expiry",
+        }
+        help_texts = {
+            "licence_number": "This is a mixture of letters and numbers, 18 characters in length and shown on the front of your driving license. Please enter it without any spaces.",
+            "licence_issue_date": "This is shown on the front of your driving licence.",
+            "licence_expiry_date": "This is shown on the front of your driving licence.",
+        }
+        widgets = {
+            "licence_issue_date": forms.widgets.DateInput(
+                attrs={"type": "date"},
+                format="%Y-%m-%d",
+            ),
+            "licence_expiry_date": forms.widgets.DateInput(
+                attrs={"type": "date"},
+                format="%Y-%m-%d",
+            ),
         }
 
     def save(self, commit=True):
@@ -65,13 +86,21 @@ class DriverProfilePart3Form(forms.ModelForm):
     class Meta:
         model = DriverProfile
         fields = ["licence_front", "licence_back"]
+        labels = {
+            "licence_front": "Picture of the front of your driving licence",
+            "licence_back": "Picture of the back of your driving licence",
+        }
+        help_texts = {
+            "licence_front": "Please upload a photo showing the whole of the front of your driving licence so the information can be clearly seen.",
+            "licence_back": "Please upload a photo showing the whole of the back of your driving licence so the information can be clearly seen.",
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            CustomImageField('licence_front'),
-            CustomImageField('licence_back'),
+            CustomImageField("licence_front"),
+            CustomImageField("licence_back"),
         )
 
     def save(self, commit=True):
@@ -91,7 +120,7 @@ class DriverProfilePart4Form(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            CustomImageField('licence_selfie'),
+            CustomImageField("licence_selfie"),
         )
 
     def save(self, commit=True):
@@ -106,6 +135,9 @@ class DriverProfilePart5Form(forms.ModelForm):
     class Meta:
         model = DriverProfile
         fields = ["licence_check_code"]
+        labels = {
+            "licence_check_code": "DVLA check code",
+        }
 
     def save(self, commit=True):
         m = super().save(commit=False)
