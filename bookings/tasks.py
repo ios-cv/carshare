83417195611a8a45cd4dd4a1099695ea78abac2b
George Goldberg <george@appletreetechnology.com>
2022-12-28 17:14:39 +0000

    Finish state machine for booking states.

--- bookings/tasks.py
+++ bookings/tasks.py
@@ -1,15 +1,19 @@
+import logging
+
 from celery import shared_task
 
 from django.utils import timezone
 
 from .models import Booking
 
+log = logging.getLogger(__name__)
+
 
 @shared_task(name="manage_booking_states")
 def manage_booking_states():
     now = timezone.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Running billing cycle at {current_time}")
+    log.info(f"Running manage_booking_states job at {current_time}")
 
     # Move active bookings that should have ended to "late" state.
     bookings = Booking.objects.filter(
@@ -18,8 +22,19 @@ def manage_booking_states():
     )
 
     for booking in bookings:
-        print(f"Marking booking as late: {booking.id}")
+        log.info(f"Marking booking as late: {booking.id}")
         booking.state = Booking.STATE_LATE
+        booking.save()
+
+    # Move pending bookings that have ended to the "ended" state.
+    bookings = Booking.objects.filter(
+        state=Booking.STATE_PENDING,
+        reservation_time__endswith__lt=now,
+    )
+
+    for booking in bookings:
+        log.info(f"Marking booking as ended: {booking.id}")
+        booking.state = Booking.STATE_ENDED
         booking.save()
 
     # Move inactive bookings that have ended to the "ended" state.
@@ -29,6 +44,6 @@ def manage_booking_states():
     )
 
     for booking in bookings:
-        print(f"Marking booking as ended: {booking.id}")
+        log.info(f"Marking booking as ended: {booking.id}")
         booking.state = Booking.STATE_ENDED
         booking.save()
