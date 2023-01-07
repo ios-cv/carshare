8893b05841351dc648fafc2c3a8a0b7ab7e4b8f3
George Goldberg <george@appletreetechnology.com>
2023-01-07 18:06:13 +0000

    Temporarily replace homepage with redirect to our web site.
    Fixes #31

--- public/views.py
+++ public/views.py
@@ -5,6 +5,9 @@ def home(request):
     if request.user.is_authenticated:
         return redirect("bookings_home")
 
+    # TODO: Remove temporary redirect here when we have proper home page content.
+    return redirect("https://www.ioscv.co.uk/carshare")
+
     context = {}
     return render(request, "public/home.html", context)
 
