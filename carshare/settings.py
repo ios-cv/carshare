408bd71339ef6b9de25a187d9fa62ddf61a6ecd8
George Goldberg <george@appletreetechnology.com>
2023-01-15 09:27:07 +0000

    Fix debug config.

--- carshare/settings.py
+++ carshare/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "nothing")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", True))
+DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 
 def env_get_list(variable, default):
