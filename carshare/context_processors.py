a3223ac006e5fec8786d53c98994f81721c42263
Scilly-guy <harry.legg86@googlemail.com>
2024-11-28 16:02:05 +0000

    removed alternative email address

--- carshare/context_processors.py
+++ carshare/context_processors.py
@@ -1,9 +1,9 @@
 from django.conf import settings
 
+
 def get_contact_info(request):
     return {
-        'CONTACT_PHONE':settings.CONTACT_PHONE,
-        'CONTACT_CARSHARE_EMAIL':settings.CONTACT_CARSHARE_EMAIL,
-        'CONTACT_EMAIL':settings.CONTACT_EMAIL,
-        'CONTACT_INTERNATIONAL_PHONE':settings.CONTACT_INTERNATIONAL_PHONE,
+        "CONTACT_PHONE": settings.CONTACT_PHONE,
+        "CONTACT_EMAIL": settings.CONTACT_CARSHARE_EMAIL,
+        "CONTACT_INTERNATIONAL_PHONE": settings.CONTACT_INTERNATIONAL_PHONE,
     }
