f9fa1b06ee2f93db40a988c7d2a1e9039a7bd45a
Scilly-guy <harry.legg86@googlemail.com>
2024-12-12 13:49:56 +0000

    updated context processor to use CONTACT_EMAIL

--- carshare/context_processors.py
+++ carshare/context_processors.py
@@ -4,6 +4,6 @@ from django.conf import settings
 def get_contact_info(request):
     return {
         "CONTACT_PHONE": settings.CONTACT_PHONE,
-        "CONTACT_EMAIL": settings.CONTACT_CARSHARE_EMAIL,
+        "CONTACT_EMAIL": settings.CONTACT_EMAIL,
         "CONTACT_INTERNATIONAL_PHONE": settings.CONTACT_INTERNATIONAL_PHONE,
     }
