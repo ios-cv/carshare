473a2c473a8d3d9dca0247abac3bef6c588e9ffb
Scilly-guy <harry.legg86@googlemail.com>
2024-12-20 18:00:38 +0000

    formatting

--- billing/tasks.py
+++ billing/tasks.py
@@ -124,9 +124,14 @@ def monthly_billing():
         invoice = stripe.Invoice.create(**invoice_kwargs)
 
         if account.business_purchase_order:
-            invoice=stripe.Invoice.modify(
+            invoice = stripe.Invoice.modify(
                 invoice.id,
-                custom_fields=[{"name":"Purchase Order","value":account.business_purchase_order},]
+                custom_fields=[
+                    {
+                        "name": "Purchase Order",
+                        "value": account.business_purchase_order,
+                    },
+                ],
             )
 
         for booking in bookings:
