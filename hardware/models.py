f5da84b2079ba0acf6716e70400d2501233b62a6
Scilly-guy <harry.legg86@googlemail.com>
2025-02-10 13:01:04 +0000

    display uptime nicer

--- hardware/models.py
+++ hardware/models.py
@@ -246,6 +246,8 @@ class Telemetry(models.Model):
         db_table = "telemetry"
 
     def free_heap_bytes_to_str(self):
+        if self.box_free_heap_bytes is None:
+            return None
         units = ["bytes", "KiB", "MiB", "GiB"]
         free_bytes = self.box_free_heap_bytes
         unit_index = 0
@@ -255,7 +257,44 @@ class Telemetry(models.Model):
         return f"{free_bytes:.2f} {units[unit_index]}"
 
     def uptime_to_str(self):
-        return str(timedelta(self.box_uptime_s))
+        if self.box_uptime_s is None:
+            return None
+        seconds_in_an_hour=60*60
+        seconds_in_a_day=24*seconds_in_an_hour
+        seconds_in_a_month=30*seconds_in_a_day
+        seconds_in_a_year=365*seconds_in_a_day
+        years, seconds_remaining=divmod(self.box_uptime_s,seconds_in_a_year)
+        months, seconds_remaining=divmod(seconds_remaining,seconds_in_a_month)
+        days, seconds_remaining=divmod(seconds_remaining,seconds_in_a_day)
+        hours, seconds_remaining=divmod(seconds_remaining,seconds_in_an_hour)
+        minutes, seconds_remaining=divmod(seconds_remaining,60)
+        seconds=int(seconds_remaining)
+        if years>0:
+            years_str=f"{years} years, "
+        else:
+            years_str=""
+        if months>0:
+            months_str=f"{months} months, "
+        else:
+            months_str=""
+        if days>0:
+            days_str=f"{days} days, "
+        else:
+            days_str=""
+        if hours>0:
+            hours_str=f"{hours} hours, "
+        else:
+            hours_str=""
+        if minutes>0:
+            minutes_str=f"{minutes} minutes, "
+        else:
+            minutes_str=""
+        if seconds>0:
+            seconds_str=f"and {seconds} seconds"
+        else:
+            seconds_str=""
+        
+        return f"{years_str}{months_str}{days_str}{hours_str}{minutes_str}{seconds_str}"
 
     def __str__(self):
         return f"""telemetry for box {self.box.id} at {self.created_at}: 
